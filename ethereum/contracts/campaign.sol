021977c14ad03a3a3d10e702e0cb9d8120888316
rheeger <rheeger@gmail.com>
2019-04-04 20:36:22 -0700

    getting the contract scaffolding started

---
+++ ethereum/contracts/campaign.sol
@@ -0,0 +1,106 @@
+pragma solidity ^0.4.25;
+
+contract DAFfactory {
+    DAF[] public deployedDAFs;
+
+    function createDAF(uint minimum) public {
+        DAF newDAF = new DAF(minimum, msg.sender, name);
+        deployedDAFs.push(newDAF);
+    }
+
+    function getDeployedDAFs() public view returns (DAF[] memory) {
+        return deployedDAFs;
+    }
+
+
+    function create501c3(string memory name, string memory taxId) public {
+        501c3 new501c3 = new 501c3(name, taxId);
+        deployed501c3s.push(new501c3);
+    }
+    function getDeployed501c3s() public view returns (DAF[] memory) {
+        return deployed501c3s;
+    }
+    
+}
+
+
+
+contract DAF {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    Request[] public requests;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    constructor(uint minimum, address creator, string memory name) public {
+        manager = creator;
+        minimumContribution = minimum;
+        DAFname = name
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution); 
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string memory description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount/2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+    function getSummary() public view returns (uint, uint, uint, uint, address) {
+        return (
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
+    }
+}
